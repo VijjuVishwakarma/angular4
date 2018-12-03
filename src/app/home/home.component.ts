8997d33cdb4412bd06ff36d4c27f32febf836c8a
vijjuVishwakarma <vijju.vijay.745@gmail.com>
2018-12-02 22:20:16 -0800

    added routing logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public actorpath:String = './actor';
+  public actresspath:String = './actress';
   constructor() { }
 
   ngOnInit() {
